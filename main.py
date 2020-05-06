b0bea771be61eb44266fbabfaeca0f12e8d08118
mvaario <mika.vaario@gmail.com>
2020-05-06 21:43:29 +0300

    - Update

--- main.py
+++ main.py
@@ -7,21 +7,15 @@
 # Added script to drive without Ai training (settings.py ai = False and ai_record = False)
 # made by mvaario
 
-from settings import *
-from sprites import *
-from keys import *
-from line_calculations import *
-from grabscreen import grab_screen
+from data_balance import *
+from drive import *
+from screen import *
 import time
 from getkeys import key_check
 import cv2
-import numpy as np
-import pygame as pg
-import pyvjoy
 
 class main:
     def __init__(self):
-        self.clock = pg.time.Clock()
 
         # x/y-axis average
         self.x = 0
@@ -32,275 +26,113 @@ class main:
         self.brake = 0
         self.old_brake = 0
 
+        # Aggression
+        self.ag = 50
+
         # speed
         self.v = 1
 
         return
 
     # Start / Recording game window
-    def start(self):
-        # Taking screen shot from game window
-        video = main.grab_screen()
-        # Resizing the window
-        video = cv2.resize(video, (851, 349))
-        # Making copy of the window
-        video_copy = np.copy(video)
+    def analyse(self):
+        video, video_copy = screen.record()
+
+        finding_lane.turn_start(self, video, video_copy)
+        finding_lane.brake_start(self, video, video_copy)
+        finding_lane.speed_start(self, video, video_copy)
 
         return video, video_copy
 
-    # Record game window
-    def grab_screen(self):
+    # Data balance
+    def data(self):
 
-        global video
-        # If user is using dual monitor setup
-        if dual_monitor:
-            # Checking ai from settings
-            if ai and ai_record:
-                video = grab_screen(region=(width, height, width + 1180, height + 349))
+        # input = loading input data ([x, slope, brake, old_brake, v])
+        input = np.load('input_data.npy', allow_pickle=True)
+        # output_x = loading recorded inputs (turn)
+        output_x = np.load('output_x.npy', allow_pickle=True)
+        # output_x = loading recorded inputs (gas, brake)
+        output_y = np.load('output_y.npy', allow_pickle=True)
+
+        input_x = input
+        input_y = input
+
+        # X-axis balance (len, shape)
+        input_x, output_x = data.x_axis(input_x, output_x)
+
+        # Y-axis balance (len, shape)
+        input_y, output_y = data.y_axis(input_y, output_y)
+
+        # model X-axis
+        data.model_x(input_x, output_x)
+
+        # model Y-axis
+        data.model_y(input_y, output_y)
+
+        print("Full data lenght", len(input))
+        print("Turning data", len(input_x))
+        print("Straight data", len(input_y))
+
+        return
+
+    # Drive controls
+    def control(self):
+        #   SCRIPT KEYS
+        if ai is True:
+            if record is True:
+                ai_record.model_record(self, input_data, output_data_x, output_data_y)
             else:
-                video = grab_screen(region=(-1700, 400, -850, 770))
-            # Converting colors BGR to RGB
-            video = cv2.cvtColor(video, cv2.COLOR_BGR2RGB)
-        return video
-
-    # Finding turning, braking lines and speed
-    def finding_lines(self, video, video_copy):
-        x = main.x
-        y = main.y
-        v = main.v
-
-        # TURN
-        # Region to find green pixels
-        region = bestline.region_of_interest_turn(video, video_copy, x)
-        # Finding green pixels
-        green_pixels = bestline.green_pixels(region)
-
-        # BRAKE
-        # Region to find red pixels
-        region = brakeline.region_of_interest_brake(video, video_copy, x, y, v)
-        # Finding red pixels
-        red_pixels = brakeline.red_pixels(region)
-
-        #   SPEED
-        # Finding speed meter position
-        region = speedometer.region_of_interest_speed(video, video_copy)
-        # Finding white pixels
-        v = speedometer.white_pixels(region)
-        main.v = v
-
-        # Drawing lines
-        lines = main.drawing_lines(video, green_pixels, red_pixels)
-
-        return lines
-
-    # Line Drawing
-    def drawing_lines(self, video, green_pixels, red_pixels):
-        old_brake = main.old_brake
-        v = main.v
-        x = main.x
-        slope = main.slope
-
-        # Drawing lines using green pixels
-        video, lines = drawing.bestline(video, green_pixels)
-
-        # Drawing lines using red pixels
-        braking, old_brake = drawing.brakeline(video, red_pixels, x, v, slope, old_brake)
-
-        # Saving braking
-        main.brake = braking
-        main.old_brake = old_brake
-
-        return lines
-
-    # Calculations
-    def line_calculations(self, lines):
-        x = main.x
-        y = main.y
-        slope = main.slope
-
-        # Average turning line
-        x, y = calculations.average_line(video, lines, x, y)
-        # Average slope
-        slope = calculations.slope(x, y, slope)
-
-        # Saving x, y and slope
-        main.x = x
-        main.y = y
-        main.slope = slope
-        return
-
-    # Define driving mode
-    def driving_mode(self):
-        if not ai and not ai_record:
-            # Running the script keys
-            main.script()
-        elif ai and ai_record:
-            # Recording data
-            main.record()
+                ai_drive.model_drive(self, model_x, model_y)
+            pass
         else:
-            # Model drive
-            main.ai_drive()
+            script.start(self)
 
         return
 
-    # Keys
-    def script(self):
-        x = main.x
-        slope = main.slope
-        v = main.v
-        brake = main.brake
-        old_brake = main.old_brake
-
-        # Changing x-axis
-        vjoy = keys.turn(x, slope, v)
-
-        # If braking lines were found
-        if brake != 0 or old_brake != 0:
-            y = keys.brake(brake, old_brake)
-        else:
-            # Changing y-axis
-            y = keys.gas(x, slope, v)
-
-        # Sending turing to virtual joystick
-        y = int(y)
-        vjoy.data.wAxisY = 0x0 + y
-
-        # Updating vjoy
-        vjoy.update()
-        return
-
-    # TENSORFLOW
-    # Recording data
-    def record(self):
-        pygame.event.pump()
-        x = main.x
-        slope = main.slope
-        v = main.v
-        brake = main.brake
-        old_brake = main.old_brake
-
-        # Getting info from screen
-        input_data.append([x, slope, brake, old_brake, v])
-
-        # Getting users turning info
-        left, straight, right = keys.x_position()
-        output_data_x.append([left, straight, right])
-
-        # Getting users gas/brake info
-        gas, zero, brake = keys.y_position()
-        output_data_y.append([gas, zero, brake])
-
-        # Saving data
-        if len(input_data) % sample_rate == 0:
-            print("Frames", len(input_data))
-            if save:
-                np.save("input_data", input_data)
-                np.save("output_x", output_data_x)
-                np.save("output_y", output_data_y)
-
-        return
-
-    # models driving
-    def ai_drive(self):
-        x = main.x
-        slope = main.slope
-        v = main.v
-        brake = main.brake
-        old_brake = main.old_brake
-
-        # Changing input shape
-        input = ai.input_shape(x, slope, v, brake, old_brake)
-
-        # Predicting x-axis
-        x_axis = keys.model_turn(model_x, input)
-
-        # Predicting y-axis
-        y_axis = keys.model_gas(model_y, input, brake, old_brake)
-
-        # Sending predictions to joystick
-        keys.model_update(x_axis, y_axis)
-
-        return
-
-    # SETUPS
-    # Displaying video and FPS
-    def setups(self, last_time):
-        # Locking FPS
-        main.clock.tick(FPS)
-        # Displaying
-        if display:
-            cv2.imshow(win_name, video)
-            if ai and ai_record:
-                cv2.moveWindow(win_name, -880, 40)
-                wait_key = 1
-            else:
-                # cv2.moveWindow(win_name, 1055, 40)
-                cv2.moveWindow(win_name, -880, 40)
-                wait_key = 1
-
-        # Printing fps, note waitkey
-        if display_fps:
-            print(round(1 / (time.time() - last_time)))
-            last_time = time.time()
-
-        return wait_key, last_time
 
 
 if __name__ == '__main__':
-
     print("")
     main = main()
-    pg.init()
     last_time = time.time()
     check_key = key_check()
-    vjoy = pyvjoy.VJoyDevice(1)
 
-    # Model record
-    if ai and ai_record:
-        # Printing recording device
-        ai.record_setup()
+    if data_balance is True:
+        main.data()
+    else:
+        if ai is True:
+            if record is True:
+                input_data, output_data_x, output_data_y = options.ai_record()
+                print("")
+            else:
+                model_x, model_y = options.ai_models()
+                print("")
+        else:
+            print("Script drive")
+            print("")
 
-        # input data all
-        input_data = []
-        # turn
-        output_data_x = []
-        # gas / brake
-        output_data_y = []
+        print("F to start")
+        while not running:
+            if 'F' in key_check():
+                print("Running")
+                print("Q to break")
+                while True:
+                    check_key = key_check()
 
-    # Model loading
-    elif ai and not ai_record:
-        model_x, model_y = ai.model_setup()
+                    # Start
+                    video, video_copy = main.analyse()
 
-    print("F to start")
-    F = 0
-    while F == 0:
-        # if 'F' in key_check():
-        F = 1
-        print("Running")
-        print("Q to break")
-        while True:
-            check_key = key_check()
+                    main.control()
 
-            # Start
-            video, video_copy = main.start()
+                    # Setup
+                    last_time = options.setups(last_time, video)
 
-            # Finding lines
-            lines = main.finding_lines(video, video_copy)
 
-            # Calculations
-            main.line_calculations(lines)
-
-            # Keys
-            main.driving_mode()
-
-            # Setup
-            wait_key, last_time = main.setups(last_time)
-
-            if cv2.waitKey(wait_key) & 0xFF == ord('q'):
-                break
-            if 'Q' in check_key:
-                F = 0
-                print("F to continue")
-                break
-        cv2.destroyAllWindows()
-        vjoy.reset()
+                    if cv2.waitKey(wait_key) & 0xFF == ord('q'):
+                        break
+                    if 'Q' in check_key:
+                        F = 0
+                        print("F to continue")
+                        break
+                cv2.destroyAllWindows()
+                vjoy.reset()
--- settings.py
+++ settings.py
@@ -1,19 +1,83 @@
+import time
+import cv2
+import pygame
+from tensorflow import keras
+
+
 # Display settings
 win_name = "F1 Screen"
 dual_monitor = True
 width = 1920
 height = 1080
+running = False
 
-FPS = 100
+wait_key = 30
 display = True
 display_fps = False
 
 # AI setup
 ai = True
-ai_record = False
-sample_rate = 250
+record = False
+sample_rate = 20
 save = False
+data_balance = False
 
 # Screen position
 width = width - 1550
 height = height - 626
+
+# Balance
+
+class options:
+
+    # Recording settings
+    def ai_record():
+
+        print("AI is True")
+        # Printing recording device
+        print("Recording inputs")
+        pygame.display.init()
+        pygame.joystick.init()
+        pygame.joystick.Joystick(0).init()
+
+        # Prints the joystick's name
+        JoyName = pygame.joystick.Joystick(0).get_name()
+        print("Recording device:", JoyName)
+        print("Saple rate", sample_rate)
+
+        # input data all
+        input_data = []
+        # turn
+        output_data_x = []
+        # gas / brake
+        output_data_y = []
+
+        return input_data, output_data_x, output_data_y
+
+    # Loading models
+    def ai_models():
+        model_x = keras.models.load_model("x_axis.h5")
+        model_y = keras.models.load_model("y_axis.h5")
+        print("AI is True")
+        print("Loading models")
+
+        return model_x, model_y
+
+    # SETUPS
+    # Displaying video and FPS
+    def setups(last_time, video):
+        # Displaying
+        if display:
+            cv2.imshow(win_name, video)
+            if ai and record:
+                cv2.moveWindow(win_name, -880, 40)
+            else:
+                # cv2.moveWindow(win_name, 1055, 40)
+                cv2.moveWindow(win_name, -880, 40)
+
+        # Printing fps, not settings wait_key
+        if display_fps:
+            print(round(1 / (time.time() - last_time)))
+            last_time = time.time()
+
+        return last_time
